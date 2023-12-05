e3b8e9d5418bef49f9bc981a657e27eac4ab4046
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-12-05 19:30:52 +0600

    I added some files

---
+++ PageDesign/PageLogin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PageLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
