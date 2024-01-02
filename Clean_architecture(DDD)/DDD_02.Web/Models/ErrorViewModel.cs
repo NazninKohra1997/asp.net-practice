730074f3749261d3ef49d78b8b1b527c8d477ca9
NazninKohra1997 <jhora.eceiu@gmail.com>
2024-01-02 23:29:56 +0600

    I deleted some files

--- Clean_architecture(DDD)/DDD_02.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DDD_02.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
